7f6ef45470223a9d41a73a0d51cc9d05088fe6b3
MariaChuikova <112131624+VladimirIsland@users.noreply.github.com>
2022-09-04 19:02:09 +0300

    +Icons

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -1,22 +1,19 @@
-import styled from 'styled-components';
-import SecondaryButton from '../../../../../Components/SecondaryButton';
-import EditIcon from '../../../../../Icons/EditIcon';
-import RemoveIcon from '../../../../../Icons/RemoveIcon';
-import IconOnlyButton from '../../../../../Components/IconOnlyButton.js';
+import styled from "styled-components";
+import SecondaryButton from "../../../../../Components/SecondaryButton";
+import EditIcon from "../../../../../Icons/EditIcon";
+import RemoveIcon from "../../../../../Icons/RemoveIcon";
+import IconOnlyButton from "../../../../../Components/IconOnlyButton.js";
 
-const Option = styled.div`
-
-
-`;
+const Option = styled.div``;
 
 function ListElement(props) {
-    return (
-        <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
-            <Option>{props.children}</Option>
-            <SecondaryButton>Иконочка редактурки :).</SecondaryButton>
-            <SecondaryButton>Иконка удаляшек :).</SecondaryButton>
-        </div>
-    );
+  return (
+    <div className="d-flex flex-row align-items-center align-self-stretch gap-2">
+      <Option>{props.children}</Option>
+      <IconOnlyButton icon={<EditIcon />} />
+      <IconOnlyButton icon={<RemoveIcon />} />
+    </div>
+  );
 }
 
 export default ListElement;
